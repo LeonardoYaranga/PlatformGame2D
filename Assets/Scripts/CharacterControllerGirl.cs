f8f15d123fe1f44e250b95f4a0b1f2cb30feea88
LeonardoYaranga <leonardo.yaranga@gmail.com>
2024-09-30 13:24:38 -0500

    Character, movement left/right, animarion static and walking, texture map..

---
+++ Assets/Scripts/CharacterControllerGirl.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float velocity;
+    private Rigidbody2D rigidbody2D;
+    private bool isLookingRight = true;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessMovement();
+    }
+
+    void ProcessMovement()
+    {
+        float inputMovement = Input.GetAxis("Horizontal");
+
+        if (inputMovement != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else {
+            animator.SetBool("isWalking", false);
+        }
+
+
+        rigidbody2D.velocity = new Vector2(inputMovement * velocity, rigidbody2D.velocity.y);
+
+        GestionateOrientation(inputMovement);
+    }
+
+    void GestionateOrientation(float inputMovement)
+    {
+
+        if ((isLookingRight && inputMovement < 0) || (!isLookingRight && inputMovement > 0))
+        {
+            isLookingRight= !isLookingRight;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+
+    }
+}
